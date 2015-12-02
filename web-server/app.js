4507dc851ba0313e7f62dc67ca5f44772d33495e
moonrailgun <moonrailgun@gmail.com>
2015-12-02 11:31:48 +0800

    登陆与注册
    ADD 登陆与注册响应

--- web-server/app.js
+++ web-server/app.js
@@ -1,4 +1,7 @@
 var express = require('express');
+var mysql = require('./lib/dao/mysql/mysql');
+var userDao = require('./lib/dao/userDao');
+var Token = require('../shared/token');
 var app = express();
 
 //所有环境
@@ -25,6 +28,70 @@ app.configure('production', function () {
     app.use(express.errorHandler());
 });
 
-console.log("Web server has started.\nPlease log on http://127.0.0.1:3001/index.html");
+//数据库初始化
+mysql.init();
+
+//请求列表
+//登陆
+app.post('/login', function(req, res) {
+    var msg = req.body;
+
+    var username = msg.username;
+    var pwd = msg.password;
+    if (!username || !pwd) {
+        res.send({code: 500});
+        return;
+    }
+
+    userDao.getUserByName(username, function(err, user) {
+        if (err || !user) {
+            console.log('username not exist!');
+            res.send({code: 500});
+            return;
+        }
+        if (pwd !== user.password) {
+            // TODO code
+            // password is wrong
+            console.log('password incorrect!');
+            res.send({code: 501});
+            return;
+        }
+
+        console.log(username + ' login!');
+        res.send({code: 200, token: Token.create(user.id, Date.now(), secret), uid: user.id});
+    });
+});
+//注册
+app.post('/register', function(req, res) {
+    //console.log('req.params');
+    var msg = req.body;
+    if (!msg.name || !msg.password) {
+        res.send({code: 500});
+        return;
+    }
+
+    userDao.createUser(msg.name, msg.password, '', function(err, user) {
+        if (err || !user) {
+            console.error(err);
+            if (err && err.code === 1062) {
+                res.send({code: 501});
+            } else {
+                res.send({code: 500});
+            }
+        } else {
+            console.log('A new user was created! --' + msg.name);
+            res.send({code: 200, token: Token.create(user.id, Date.now(), secret), uid: user.id});
+        }
+    });
+});
+
+
 
 app.listen(3001);
+
+// Uncaught exception handler
+process.on('uncaughtException', function(err) {
+    console.error(' Caught exception: ' + err.stack);
+});
+
+console.log("Web server has started.\nPlease log on http://127.0.0.1:3001/index.html");
