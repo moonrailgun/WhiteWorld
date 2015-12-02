b883c422b4cf5c2049b2c378e92afbf7464fb947
moonrailgun <moonrailgun@gmail.com>
2015-12-02 11:31:12 +0800

    登陆与用户
    ADD 登陆相关
    ADD 用户数据逻辑

--- web-server/public/js/login.js
+++ web-server/public/js/login.js
@@ -6,6 +6,57 @@ var Login = function (serverOption) {
     var host = serverOption.ip;
     var port = serverOption.port ? serverOption.port : 3001;
 
+    var httpHost = location.href.replace(location.hash, '');
+
+    var config = {
+        GATE_HOST : window.location.hostname,
+        GATE_PORT : 3014
+    };
+
+    pomelo.on('websocket-error', function(){
+        loading = false;
+    });
+
+    this.login = function (){
+        if (loading) {
+            return;
+        }
+        loading = true;
+        var username = $('#loginUser').val().trim();
+        var $loginPwd = $('#loginPwd');
+        var pwd = $loginPwd.val().trim();
+        $loginPwd.val('');
+        if (!username) {
+            alert("Username is required!");
+            loading = false;
+            return;
+        }
+
+        if (!pwd) {
+            alert("Password is required!");
+            loading = false;
+            return;
+        }
+
+        $.post(httpHost + 'login', {username: username, password: pwd}, function(data) {
+            if (data.code === 501) {
+                alert('用户名或密码不正确');
+                loading = false;
+                return;
+            }
+            if (data.code !== 200) {
+                alert('该用户不存在');
+                loading = false;
+                return;
+            }
+
+            authEntry(data.uid, data.token, function() {
+                loading = false;
+            });
+            localStorage.setItem('username', username);
+        });
+    };
+
     //todo
     this.entry = function (name, callback) {
         pomelo.init({
@@ -19,5 +70,49 @@ var Login = function (serverOption) {
                 //todo
             });
         })
+    };
+
+    var authEntry = function(uid, token, callback){
+        queryEntry(uid, function(host, port) {
+            entry(host, port, token, callback);
+        });
+    };
+
+    //从gate服务器获取分配的服务器ip与port
+    var queryEntry = function(uid, callback){
+        pomelo.init({
+            host : config.GATE_HOST,
+            port : config.GATE_PORT,
+            log: true
+        },function(){
+            pomelo.request('gate.gateHandler.queryEntry',{uid:uid},function(data){
+                pomelo.disconnect();
+
+                if(data.code === 2001) {
+                    alert('Servers error!');
+                    return;
+                }
+
+                callback(data.host, data.port);
+            });
+        })
+    };
+
+    var entry = function(host,port,token,callback){
+        if(host === '127.0.0.1') {
+            host = config.GATE_HOST;//转义为本地
+        }
+
+        pomelo.init({
+            host:host,
+            port:port,
+            log:true
+        },function(){
+            pomelo.request('connector.entryHandler.entry'),{token:token},function(data){
+
+            }
+        })
     }
 };
+
+var login = new Login();
