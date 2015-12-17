b9550e569c1e4495adda8b868fb5a75393f03bfd
moonrailgun <moonrailgun@gmail.com>
2015-12-17 21:39:35 +0800

    ..
    ..

--- game-server/app/servers/chat/remote/chatRemote.js
+++ game-server/app/servers/chat/remote/chatRemote.js
@@ -2,33 +2,33 @@
  * Created by Chen on 2015-12-17.
  */
 
-module.exports = function(app) {
+module.exports = function (app) {
     return new ChatRemote(app);
 };
 
-var ChatRemote = function(app) {
+var ChatRemote = function (app) {
     this.app = app;
     this.channelService = app.get('channelService');
 };
 
 //添加用户到这个频道
 var remote = ChatRemote.prototype;
-remote.add = function(playerId, sessionId,channelName,callback){
+remote.add = function (playerId, sessionId, channelName, callback) {
     var channel = this.channelService.getChannel(channelName, true);
-    if(!!channel){
+    if (!!channel) {
         channel.add(playerId, sessionId);
     }
 
     callback(channel.getMembers());
 };
 //从频道中获取用户
-remote.get = function(){
+remote.get = function () {
 
 };
 //踢出用户
-remote.kick = function(playerId, sessionId, channelName, callback){
+remote.kick = function (playerId, sessionId, channelName, callback) {
     var channel = this.channelService.getChannel(channelName, false);
-    if(!!channel){
+    if (!!channel) {
         channel.leave(playerId, sessionId);
         callback();
     }
