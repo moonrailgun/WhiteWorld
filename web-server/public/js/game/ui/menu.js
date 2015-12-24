60862380649abfb4fc6ccaa2a87012b74de0fb51
moonrailgun <moonrailgun@gmail.com>
2015-12-24 20:50:27 +0800

    菜单切换
    ADD 菜单切换
    UPDATE 菜单选项布局

--- web-server/public/js/game/ui/menu.js
+++ web-server/public/js/game/ui/menu.js
@@ -8,4 +8,10 @@
         event.stopPropagation();//阻止事件冒泡
         menuContent.toggle(400);
     });
+
+    $('#menu-cell li').click(function(event){
+        var obj = this;
+        $('.actived').removeClass('actived');
+        $(obj).addClass('actived');
+    })
 })();
