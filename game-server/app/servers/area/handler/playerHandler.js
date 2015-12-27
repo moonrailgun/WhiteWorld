f02de926f01579da524237bdf76efee0e999a1cf
moonrailgun <moonrailgun@gmail.com>
2015-12-27 22:12:57 +0800

    ..
    ..

--- game-server/app/servers/area/handler/playerHandler.js
+++ game-server/app/servers/area/handler/playerHandler.js
@@ -6,12 +6,20 @@ var fs = require('fs');
 var area = require('../../../domain/area/area');
 var dataApi = require('../../../util/dataApi');
 var Player = require('../../../domain/entity/player');
+var userDao = require('../../../dao/userDao');
+var bagDao = require('../../../dao/bagDao');
 
 var handler = module.exports;
 
 handler.enterScene = function(msg,session,next){
-    var player = new Player({id:msg.playerId, playerName:msg.playerName});
+    var playerId = session.get('playerId');
+    var playerName = session.get('playerName');
+    var player = new Player({id:playerId, playerName:playerName});
     player.serverId = session.frontendId;//前端连接服务器ID
+
+    userDao.getPlayerAllInfo(playerId,function(err, player){
+        //todo
+    });
 
     var data = {
 
