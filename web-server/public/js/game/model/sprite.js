4780e6a46307f6b848051824a40ee6cc283c378c
moonrailgun <moonrailgun@gmail.com>
2016-01-15 21:30:18 +0800

    ..
    ..

--- web-server/public/js/game/model/sprite.js
+++ web-server/public/js/game/model/sprite.js
@@ -21,8 +21,8 @@ Sprite.prototype._init = function() {
     var startAin = 'Stand';
     var type = this.entity.type;
     if (type === EntityType.PLAYER || type === EntityType.MOB) {
-        this._initDynamictNode(aniOrientation.LEFT_DOWN + startAin);
+        //this._initDynamictNode(aniOrientation.LEFT_DOWN + startAin);
     } else if (type === EntityType.NPC || type === EntityType.ITEM || type === EntityType.EQUIPMENT) {
-        this._initStaticNode();
+        //this._initStaticNode();
     }
 };
